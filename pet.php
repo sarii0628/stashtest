a4f8f12bc993c7fea3ee0374944cabe329977b36
sarii0628 <9mm.p.bull@gmail.com>
2020-03-20 16:01:00 +0900

    puu -> puu and nii

--- pet.php
+++ pet.php
@@ -1,4 +1,11 @@
 <?php 
 $dog = "chocolat";
-$cat = "puu";
-echo "my pets are $dog and $cat";
+$cats = ["puu", "nii"];
+$catstr = "";
+foreach ($cats as $cat) {
+    $catstr = $catstr.$cat;
+    if($cat != end($cats)){    
+        $catstr = $catstr." and ";
+    }
+}
+echo "my pets are $dog and $catstr";
